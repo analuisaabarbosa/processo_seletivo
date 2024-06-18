f38f500a778b7ead8b320b72a092c69dd8880fcd
analuisa <analuisaaugustob@gmail.com>
2024-06-18 11:34:36 -0300

    👌style: Alterações de revisão de código.

--- controle_de_candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle_de_candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,9 +1,38 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        selecionarCandidato();
+        String [] candidatos = {"João", "Marcela", "Joana", "Reginaldo", "Elias"};
+        for (String candidato : candidatos){
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando) {
+                tentativasRealizadas++;
+            }
+            else
+                System.out.println("Contato realizado com sucesso!");
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu) {
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa." );
+        }
+        else
+            System.out.println("Não conseguimos contato com " + candidato + ", número máximo de tentativas " + tentativasRealizadas);
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
     }
 
     static void analisarCandidato(double salarioPretendido){
